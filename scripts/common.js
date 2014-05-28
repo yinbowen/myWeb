81da1427736fc9b9dde13c0544b919e17a7b3bc4
Saner <bowen_yin@163.com>
2014-05-28 10:39:38 +0800

    完成了回到顶部按钮的制作。IE6真是个爷爷，不支持fixed定位。

--- scripts/common.js
+++ scripts/common.js
@@ -6,7 +6,7 @@ $(function(){
 		var def=i.find(".s-mod-def");
 		var cur=i.find(".s-mod-cur");
 		
-		//滑动效果
+		//导航栏滑动效果
 		i.hover(function(){
 			def.stop().animate({top:-60},600,"easeOutBounce")
 			cur.stop().animate({top:-60},600,"easeOutBounce")
@@ -15,4 +15,28 @@ $(function(){
 			cur.stop().animate({top:0},600,"easeOutBounce")
 		})
 	})
+
+	 //回到顶部部分
+	 //首先将#goTopDiv隐藏 
+    $("#goTopDiv").hide()
+    .css('opacity', '0.7');
+    
+    //当滚动条的位置处于距顶部100像素以下时，跳转链接出现，否则消失
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 100) {
+            $("#goTopDiv").fadeIn(600);    
+        } else {         
+            $("#goTopDiv").fadeOut(600);
+        }
+    });
+   
+    //当点击跳转链接后，回到页面顶部位置
+    $("#goTopDiv").click(function () {
+        $("body,html").animate({
+            scrollTop: 0
+        }, 400);
+        return false;
+    })
+    
 })
+
